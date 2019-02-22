080d409d7f049fe5b77816a57e031021cf22c848
zbelachew@gmail.com <zbelachew@gmail.com>
2019-02-22 14:44:01 -0500

    Removed the Comments column from dbo.ProjectRegistries table

--- UnifiedStage/Tables/dbo.ProjectRegistries.sql
+++ UnifiedStage/Tables/dbo.ProjectRegistries.sql
@@ -6,9 +6,8 @@ CREATE TABLE [dbo].[ProjectRegistries]
 [UpdatedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [CreatedDate] [datetime] NOT NULL CONSTRAINT [DF_ProjectRegistries_CreatedDate] DEFAULT (sysdatetime()),
 [UpdatedDate] [datetime] NOT NULL CONSTRAINT [DF_ProjectRegistries_UpdatedDate] DEFAULT (sysdatetime()),
-[Action] [varchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Comments] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+[Action] [varchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[ProjectRegistries] ADD CONSTRAINT [ProjectRegistryId_PK] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
 GO
